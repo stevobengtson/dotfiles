ab3e0bf64718db4fd5b9938a424f4588d2972a36
Steven Bengtson <steven.bengtson@gmail.com>
2024-05-08 23:18:26 -0700

    Add autosave and tmux navigator
    Autosave on leaving insert mode or text changes
    navigate tmux panes with ctrl + h,j,k or l

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Autosave file when leaving insert mode or when the text has changed
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  pattern = { "*" },
+  command = "silent! wall",
+  nested = true,
+})
