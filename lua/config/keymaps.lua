d5db1b5f36a60f8bea017b4589ec2a4a7859289d
Steven Bengtson <steven.bengtson@gmail.com>
2024-05-09 23:03:42 -0700

    Add dadbod and rest client
    Dadbod is an sql client.
    Rest client is to make http calls.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 'DBUI',
+-- 'DBUIToggle',
+-- 'DBUIAddConnection',
+-- 'DBUIFindBuffer',
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>zu", "<cmd>DBUI<cr>", { desc = "Database UI" })
+map("n", "<leader>zt", "<cmd>DBUIToggle<cr>", { desc = "Database UI Toggle" })
+map("n", "<leader>zc", "<cmd>DBUIAddConnection<cr>", { desc = "Database UI Add connection" })
+map("n", "<leader>zf", "<cmd>DBUIFindBuffer<cr>", { desc = "Database UI Find buffer" })
+
+map("n", "<leader>rr", "<cmd>Rest run<cr>", { desc = "Run rest command" })
