ff70e60248321e2c27bb557f3f1290bfca01956d
Steven Bengtson <sbengtson@learntowin.com>
2024-05-15 14:47:30 -0700

    Remove transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,7 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      transparent = true,
       style = "moon",
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
     },
   },
 }
