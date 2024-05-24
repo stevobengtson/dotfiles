73f7202e3d27f2a109d10ea5773d9e5c59ccf5f7
Steven Bengtson <steven.bengtson@gmail.com>
2024-05-24 09:00:07 -0700

    Update tmux and add tdo

---
+++ .config/nvim/lua/plugins/tdo.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "2kabhishek/tdo.nvim",
+    dependencies = "nvim-telescope/telescope.nvim",
+    cmd = { "Tdo", "TdoEntry", "TdoNote", "TdoTodos", "TdoToggle", "TdoFind", "TdoFiles" },
+    keys = { "[t", "]t" },
+  },
+}
